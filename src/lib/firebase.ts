a6eb7fba93bfbd434869bbd60ae5e73cc80f44af
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2025-02-12 23:13:31 +0100

    added taking quests live from db firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
@@ -19,3 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
 export const storage = getStorage(app);
+export const db = getFirestore(app);
